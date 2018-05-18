02259666fe2e3eaae5c370a9b4cc222fb3c5a5e1
liumingming <mingmingsuper@gmail.com>
2018-05-18 10:04:07 +0800

    新建文件

--- app/src/main/java/com/lmm/jobschedulerdemo/MainActivity.java
+++ app/src/main/java/com/lmm/jobschedulerdemo/MainActivity.java
@@ -1,7 +1,16 @@
 package com.lmm.jobschedulerdemo;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.lmm.jobschedulerdemo.service.JobSchedulerService;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            doService();
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void doService() {
+        JobScheduler jobScheduler = (JobScheduler)getSystemService(JOB_SCHEDULER_SERVICE);
+        JobInfo.Builder builder = new JobInfo.Builder(1, new ComponentName(this,JobSchedulerService.class));
+        builder.setMinimumLatency(TimeUnit.MILLISECONDS.toMillis(10));
+        builder.setOverrideDeadline(TimeUnit.MILLISECONDS.toMillis(15));
+        builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_NOT_ROAMING);
+        builder.setBackoffCriteria(TimeUnit.MILLISECONDS.toMillis(10),JobInfo.BACKOFF_POLICY_LINEAR);
+        builder.setRequiresCharging(true);
+        jobScheduler.schedule(builder.build());
     }
 }
--- app/src/main/java/com/lmm/jobschedulerdemo/service/JobSchedulerService.java
+++ app/src/main/java/com/lmm/jobschedulerdemo/service/JobSchedulerService.java
@@ -1,21 +1,46 @@
 package com.lmm.jobschedulerdemo.service;
 
+import android.annotation.TargetApi;
+import android.app.job.JobInfo;
 import android.app.job.JobParameters;
+import android.app.job.JobScheduler;
 import android.app.job.JobService;
+import android.content.ComponentName;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Message;
 import android.widget.Toast;
 
+import java.util.concurrent.TimeUnit;
+
+@TargetApi(Build.VERSION_CODES.LOLLIPOP)
 public class JobSchedulerService extends JobService {
 
     @Override
     public boolean onStartJob(JobParameters params) {
+        doJob();
+        doService();
         return false;
     }
 
     @Override
     public boolean onStopJob(JobParameters params) {
         return false;
+    }
+
+    private void doService() {
+        JobScheduler jobScheduler = (JobScheduler)getSystemService(JOB_SCHEDULER_SERVICE);
+        JobInfo.Builder builder = new JobInfo.Builder(1, new ComponentName(this,JobSchedulerService.class));
+        builder.setMinimumLatency(TimeUnit.MILLISECONDS.toMillis(10));
+        builder.setOverrideDeadline(TimeUnit.MILLISECONDS.toMillis(15));
+        builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_NOT_ROAMING);
+        builder.setBackoffCriteria(TimeUnit.MILLISECONDS.toMillis(10),JobInfo.BACKOFF_POLICY_LINEAR);
+        builder.setRequiresCharging(true);
+        jobScheduler.schedule(builder.build());
+    }
+
+    private void doJob() {
+        Toast.makeText(getApplicationContext(), "测试", Toast.LENGTH_SHORT).show();
     }
 
     private Handler mJobHandler = new Handler(new Handler.Callback() {
